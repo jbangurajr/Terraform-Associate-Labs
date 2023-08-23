c8850cf3f8d56d6aada2e4f25afdbd8704ccb73d
Johnathan Bangura <jbangura@vt.edu>
2023-08-22 23:59:15 -0400

    052

--- 052-resource-meta-arguments-for-each/main.tf
+++ 052-resource-meta-arguments-for-each/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.58.0"
+      version = "5.13.1"
     }
   }
 }
@@ -11,19 +11,23 @@ provider "aws" {
   profile = "default"
   region  = "us-east-1"
 }
+
+
 resource "aws_instance" "my_server" {
-	for_each = {
-		nano = "t2.nano"
-		micro =  "t2.micro"
-		small =  "t2.small"
-	}
+  for_each = {
+    nano = "t2.nano"
+    micro = "t2.micro"
+    small = "t2.small"
+  }
   ami           = "ami-087c17d1fe0178315"
   instance_type = each.value
-	tags = {
-		Name = "Server-${each.key}"
-	}
+  tags = {
+    Name = "Server-${each.key}"
+  }
+
 }
 
-output "public_ip" {
+
+output "public_ip_all" {
   value = values(aws_instance.my_server)[*].public_ip
 }
