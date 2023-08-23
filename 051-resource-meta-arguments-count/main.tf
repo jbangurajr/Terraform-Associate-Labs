bba8f1d2d326a227547b08d58247892e050c9f64
Johnathan Bangura <jbangura@vt.edu>
2023-08-22 23:49:19 -0400

    051

--- 051-resource-meta-arguments-count/main.tf
+++ 051-resource-meta-arguments-count/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.58.0"
+      version = "5.13.1"
     }
   }
 }
@@ -11,15 +11,27 @@ provider "aws" {
   profile = "default"
   region  = "us-east-1"
 }
+
+
 resource "aws_instance" "my_server" {
-	count = 2
+  count         = 2
   ami           = "ami-087c17d1fe0178315"
   instance_type = "t2.micro"
-	tags = {
-		Name = "Server-${count.index}"
-	}
+  tags = {
+    Name = "Server-${count.index}"
+  }
+
 }
 
-output "public_ip" {
+
+output "public_ip0" {
+  value = aws_instance.my_server[0].public_ip
+}
+
+output "public_ip1" {
+  value = aws_instance.my_server[1].public_ip
+}
+
+output "public_ipboth" {
   value = aws_instance.my_server[*].public_ip
 }
