e689da26202e1429076e56847c7b90b4576aaa16
Johnathan Bangura <jbangura@vt.edu>
2023-08-23 00:30:33 -0400

    054

--- 054-resource-meta-arguments-lifecycle/main.tf
+++ 054-resource-meta-arguments-lifecycle/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.58.0"
+      version = "5.13.1"
     }
   }
 }
@@ -11,17 +11,32 @@ provider "aws" {
   profile = "default"
   region  = "us-east-1"
 }
+
+
 resource "aws_instance" "my_server" {
+  count         = 2
   ami           = "ami-087c17d1fe0178315"
   instance_type = "t2.micro"
-	tags = {
-		Name = "My-Server"
-	}
-	lifecycle {
-		prevent_destroy = false
-	} 
+  tags = {
+    Name = "Server-${count.index}"
+  }
+  lifecycle {
+    prevent_destroy       = false
+    create_before_destroy = true
+
+  }
+
 }
 
-output "public_ip" {
-  value = aws_instance.my_server.public_ip
+
+output "public_ip0" {
+  value = aws_instance.my_server[0].public_ip
+}
+
+output "public_ip1" {
+  value = aws_instance.my_server[1].public_ip
+}
+
+output "public_ipboth" {
+  value = aws_instance.my_server[*].public_ip
 }
