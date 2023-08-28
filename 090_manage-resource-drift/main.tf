94571365bccaf43fde2d6d3963a97a93e7210fbf
Johnathan Bangura <jbangura@vt.edu>
2023-08-28 19:08:04 -0400

    090

--- 090_manage-resource-drift/main.tf
+++ 090_manage-resource-drift/main.tf
@@ -1,28 +1,5 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.58.0"
-    }
-  }
-}
+terraform apply -replace=aws_instance.my_server
 
-provider "aws" {
-  profile = "default"
-  region  = "us-east-1"
-}
-resource "aws_instance" "my_server" {
-  ami           = "ami-087c17d1fe0178315"
-  instance_type = "t2.micro"
-	tags = {
-		Name = "MyServer"
-	}
-}
+terraform apply -refresh-only
 
-resource "aws_s3_bucket" "bucket" {
-  bucket = "my-new-bucket-421419084210"
-}
-
-output "public_ip" {
-  value = aws_instance.my_server.public_ip
-}
+terraform import aws_s3_bucket.<resource_name> <bucket_name>
