e5bdc81b94441c9f356cd9e983bd88cb91cd1f9f
Johnathan Bangura <jbangura@vt.edu>
2023-08-28 19:28:45 -0400

    issues

--- 100_troubleshooting/main.tf
+++ 100_troubleshooting/main.tf
@@ -5,3 +5,5 @@ TFLOG=trace TFLOG_PATH=terraform.log terraform apply
 TFLOGCORE=trace terraform apply
 
 TFLOGPROVIDER=trace terraform apply
+
+https://github.com/hashicorp/terraform/issues/new/choose
