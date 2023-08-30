95b122d492c8067a44ebceab69175546682ea1d3
Johnathan Bangura <jbangura@vt.edu>
2023-08-29 23:09:31 -0400

    130

--- 130_backends-standard-s3/main.tf
+++ 130_backends-standard-s3/main.tf
@@ -1,29 +1,29 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-backend-4329408"
+    bucket = "terraform-backend-4329408jb"
     key    = "terraform.tfstate"
     region = "us-east-1"
-    dynamodb_table = "force-unlock-terraform"
+    encrypt = true
   }
 }
-
 
 provider "aws" {
   region = "us-east-1"
 }
 
 resource "aws_s3_bucket" "bucket" {
-	bucket = var.bucket
+  bucket = var.bucket
+  force_destroy = true
 }
 
 module "apache" {
-  source  = "ExamProCo/apache-example/aws"
-  version = "1.1.0"
-	vpc_id = var.vpc_id
-	my_ip_with_cidr = var.my_ip_with_cidr
-	public_key = var.public_key
-	instance_type = var.instance_type
- 	server_name = var.server_name
+  source          = "jbangurajr/apache-example/aws"
+  version         = "v1.0.4"
+  vpc_id          = var.vpc_id
+  my_ip_with_cidr = var.my_ip_with_cidr
+  public_key      = var.public_key
+  instance_type   = var.instance_type
+  server_name     = var.server_name
 }
 
 output "public_ip" {
--- 130_backends-standard-s3/variables.tf
+++ 130_backends-standard-s3/variables.tf
@@ -1,31 +1,24 @@
 variable "bucket" {
-	type = string
-	default = "320489324827429471210198"
+  type    = string
+  default = "terraform-backend-4329408jb"
 }
 
 variable "vpc_id" {
-	type = string
+  type = string
 }
 
 variable "my_ip_with_cidr" {
-	type = string
+  type = string
 }
 
 variable "public_key" {
-	type = string
+  type = string
 }
 
 variable "instance_type" {
-	type = string
+  type = string
 }
 
 variable "server_name" {
-	type = string
-}
-
-variable "workspace_iam_roles" {
-  default = {
-    staging    = "arn:aws:iam::STAGING-ACCOUNT-ID:role/Terraform"
-    production = "arn:aws:iam::PRODUCTION-ACCOUNT-ID:role/Terraform"
-  }
+  type = string
 }
