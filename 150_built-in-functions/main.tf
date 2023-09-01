b23350eda1ec639119af28e077ce72c365f0cb5c
Johnathan Bangura <jbangura@vt.edu>
2023-08-31 23:37:05 -0400

    150

--- 150_built-in-functions/main.tf
+++ 150_built-in-functions/main.tf
@@ -18,3 +18,49 @@ variable "stuff" {
 		"goodbye" = "moon"
 	}
 }
+
+/*
+
+# Built-in Functions
+terraform console
+
+
+> var.str
+""
+>
+
+
+> var.items
+tolist([
+  tostring(null),
+  tostring(null),
+  "",
+  "last",
+])
+>
+
+
+
+> var.stuff
+tomap({
+  "goodbye" = "moon"
+  "hello" = "world"
+})
+
+> keys(var.stuff)
+tolist([
+  "goodbye",
+  "hello",
+])
+
+> uuid()
+"2ac8cd41-012b-23c3-f438-355b9017fdee"
+
+
+> bcrypt("hello world")
+"$2a$10$v7qCEHwDi9g8jvIZRVFWIOcjAYZDNcv2KE6L7frM9Ldsk2ap/DQCK"
+
+> cidrsubnet("172.16.0.0/12", 4,2)
+"172.18.0.0/16"
+
+*/
